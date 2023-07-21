6f13ffd625c1cef973ae6a62eec5a055ab1eb3a1
Pascal Rocq <pascal.rocq@csgroup.eu>
2023-07-21 16:26:09 +0200

    ajout de la possibilité d'exclure des classes

--- xmlParser.py
+++ xmlParser.py
@@ -6,6 +6,24 @@ from pprint import pprint
 import argparse
 import logging
 
+classes_a_exclure = ("cmoip.ICMoIPDistant",
+                    "cmoip.ICmoipCie",
+                    "cmoip.ICmoipClient",
+                    "cmoip.ICmoipClientCie",
+                    "cmoip.ICmoipServerStandaloneCie",
+                    "cmoip.ICmoipServerStandaloneSnmp",
+                    "cmoip.ICmoipSnmp",
+                    "ithTphAnnuaire.ICorrespExterne",
+                    "mg2s.IAtelierEnergie",
+                    "mg2s.IAtelierEnergieCie",
+                    "mg2s.IAtelierEnergie"
+                    "mg2s.IAtelierEnergieCie",
+                    "mg2s.IFarCerCie"
+                    "mg2s.IFarCerSnmp"
+                    "mg2s.IFarCexCie"
+                    "mg2s.IFarCexSnmp"
+                    )
+
 def decrireNoeud(noeud, niveau=0, decrireEnfants=False):
     print("N=", niveau, "   tag=", noeud.tag, "attrib=", noeud.attrib, "len=", len(noeud))
     if len(noeud) > 0 and decrireEnfants:
@@ -35,9 +53,10 @@ args = parser.parse_args()
 #     resultFolder = "templ"
 print("filename=" + args.filename)
 #print("resultFolder=" + args.resultFolder)
-
+#print("aaaaaaaaaaa")
 resultFolder = p / (args.filename + "_templParClasse")
-listeClasses =  p / (args.filename + "_listeClasses")
+listeClasses =  p / (args.filename + "_listeClasses.txt")
+listeClassesExclues =  p / (args.filename + "_listeClassesExclues.txt")
 if not resultFolder.exists():
     resultFolder.mkdir()
     print(f"Création du dossier {resultFolder.name}")
@@ -46,6 +65,8 @@ if resultFolder.is_file():
     exit(1)
 if not listeClasses.exists():
     listeClasses.touch()
+if not listeClassesExclues.exists():
+    listeClassesExclues.touch()
 tree = et.parse(args.filename)
 root = tree.getroot()
 
@@ -54,19 +75,30 @@ root = tree.getroot()
 # print(type(resultFolder))
 groupObjectModel = root[0][0]
 texteListeClasses = ""
+texteListeClassesExclues = ""
 nbClasses = 0
+nbClassesExclues = 0
 for enfant in groupObjectModel:
-    nbClasses += 1
     class_name = enfant.get('class-name')
-    texteListeClasses += "\n" + class_name
-    filename = resultFolder / ("templ_" + class_name + ".xml")
-    #print(f"======  {filename.name} {filename.stem} {filename.suffix} {filename.parts}")
-    filename.write_text(xml.etree.ElementTree.tostring(enfant, encoding='unicode'))
-    # with open(filename, "w", encoding="utf-8") as f:
-    #     f.write(xml.etree.ElementTree.tostring(enfant, encoding='unicode'))
+    if class_name in classes_a_exclure:
+        nbClassesExclues += 1
+    else:
+        nbClasses += 1
+        #print (class_name)
+        #if class_name == "ithTphConfiguration.IDomains":
+        #    print (enfant)
+        texteListeClasses += "\n" + class_name
+        filename = resultFolder / ("templ_" + class_name + ".xml")
+        #print(f"======  {filename.name} {filename.stem} {filename.suffix} {filename.parts}")
+        enfant_str = xml.etree.ElementTree.tostring(enfant, encoding='unicode')
+        filename.write_text(enfant_str, encoding='utf-8')
+        # with open(filename, "w", encoding="utf-8") as f:
+        #     f.write(xml.etree.ElementTree.tostring(enfant, encoding='unicode'))
 listeClasses.write_text(texteListeClasses)
+listeClassesExclues.write_text(texteListeClassesExclues)
 print(f"{nbClasses} classes trouvées")
-    
+print(f"{nbClassesExclues} classes exclues")
+
 if False:
     print("-----------------------------")
     #decrireNoeud(root, 0, True)
